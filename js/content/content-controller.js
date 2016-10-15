3b09e9d6da2aa138827dd569af82b497faca6e51
Erik Gomez <egoxiv@gmail.com>
2016-10-15 13:18:40 -0700

    Cleans up HTML and CSS, left aligns copy block

--- js/content/content-controller.js
+++ js/content/content-controller.js
@@ -1,10 +1,10 @@
 module.exports = function(myApp) {
   myApp.controller('ContentController', [ '$scope', ($scope) => {
     $scope.headline = 'Hi, I\'m Erik.';
-    $scope.byline = 'Creative thinker, JavaScript whisperer, and Web Developer in Bakersfield, CA';
+    $scope.byline = 'Web developer and JavaScript whisperer in Bakersfield, CA. My site is currently under construction. Please contact me in the meantime.';
 
     class CTA {
-      constructor(text, src = null, mailTo = null, logo = null) {
+      constructor(text, src, mailTo, logo) {
         this.text = text;
         this.logo = logo;
         this.hyperText = {
